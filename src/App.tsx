eff2f7a65997a8d41723e131c9cf7a3532a219df
Prabhat-Mani <prabhat@vcbay.co>
2023-07-03 00:46:51 +0530

    Todo adding functionality done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
-function App() {
-  return <div className="App">Hello</div>;
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo("")
+    }
+  }
+
+  return <div className="App">
+    <span className="heading">Taskify</span>
+    <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+    <TodoList/>
+  </div>;
 }
 
 export default App;
